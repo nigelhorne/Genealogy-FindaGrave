6bc07d73897318dcd211bc32841d9e1d40812957
Nigel Horne <njh@bandsman.co.uk>
2017-06-19 20:26:31 -0400

    Release 0.04

--- lib/WWW/Scrape/FindaGrave.pm
+++ lib/WWW/Scrape/FindaGrave.pm
@@ -13,11 +13,11 @@ WWW::Scrape::FindaGrave - Scrape the Find a Grave website
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
